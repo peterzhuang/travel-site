82f760da64f54343ced6a8d4caa282e14806040e
yuchuan <yuchuan@gmail.com>
2017-02-13 10:06:44 -0500

    completed hero and button block

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,9 @@ watch = require('gulp-watch'),
 postcss = require('gulp-postcss'),
 autoprefixer = require('autoprefixer'),
 cssvars = require('postcss-simple-vars'),
-nested = require('postcss-nested');
+nested = require('postcss-nested'),
+cssImport = require('postcss-import');
+
 gulp.task('default',function() {
 	console.log("yeah - you created gulp task.");
 });
@@ -13,7 +15,7 @@ gulp.task('html', function() {
 });
 gulp.task('styles', function() {
 	return gulp.src('./app/assets/styles/styles.css')
-		.pipe(postcss([cssvars,nested,autoprefixer]))
+		.pipe(postcss([cssImport,cssvars,nested,autoprefixer]))
 		.pipe(gulp.dest('./app/temp/styles'));
 });
 
